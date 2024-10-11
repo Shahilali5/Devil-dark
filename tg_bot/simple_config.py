66928e7c1f40971181f420f3ee0c9f73618f72d5
SHAHIL444 <110395532+Shahilali5@users.noreply.github.com>
2024-10-11 08:14:53 +0530

    Update simple_config.py

--- tg_bot/simple_config.py
+++ tg_bot/simple_config.py
@@ -12,10 +12,10 @@ class Config(object):
     # REQUIRED
     API_KEY = "YOUR KEY HERE"
     OWNER_ID = "YOUR ID HERE"  # If you dont know, run the bot and do /id in your private chat with it
-    OWNER_USERNAME = "YOUR USERNAME HERE"
+    OWNER_USERNAME = "Shahil440"
 
     # RECOMMENDED
-    SQLALCHEMY_DATABASE_URI = 'sqldbtype://username:pw@hostname:port/db_name'  # needed for any database modules
+    SQLALCHEMY_DATABASE_URI = 'mongodb+srv://devil:devil@dark@cluster0.io6tl.mongodb.net/'  # needed for any database modules
     MESSAGE_DUMP = None  # needed to make sure 'save from' messages persist
     LOAD = []
     NO_LOAD = ['translation', 'rss']
